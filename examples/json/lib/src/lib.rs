360ae9bae5fe01d65ffb745ccdb059c6c65ec642
Tsahi Zidenberg <tsahi@offchainlabs.com>
2023-08-29 16:05:30 -0600

    example/json: use json instead of serde_json

--- examples/json/lib/src/lib.rs
+++ examples/json/lib/src/lib.rs
@@ -4,26 +4,45 @@ extern crate alloc;
 use alloc::vec::Vec;
 use alloc::string::String;
 
-use serde_json::Value;
-
 pub fn parse_json(input: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
     let input = String::from_utf8_lossy(&input);
     let (path, json) = input.split_once(":").unwrap();
-    let mut value: Value = serde_json::from_str(json).unwrap();
-    for elem in path.split("|") {
-        if let Ok(index) = elem.parse::<usize>() {
-                value = value.get_mut(index).unwrap().take();
-                continue;
-        }
-        if elem.len() > 0 && elem.chars().nth(0).unwrap() == '\"' && elem.chars().last().unwrap() == '\"' {
-            let fieldname = &elem[1..elem.len()-1];
-            value = value[fieldname].take();
+    let orig = json::parse(json).unwrap();
+    let mut value = &orig;
+    for path_elem in path.split("|") {
+        if let Ok(index) = path_elem.parse::<usize>() {
+            let mut i :usize = 0;
+            for elem in value.members() {
+                if i == index {
+                    value = elem;
+                    break;
+                }
+                i+=1;
+            }
+            if i != index {
+                panic!("invalid index");
+            }
             continue;
         }
-        if elem == "String" {
+        if path_elem.len() > 0 && path_elem.chars().nth(0).unwrap() == '\"' && path_elem.chars().last().unwrap() == '\"' {
+            let fieldname = &path_elem[1..path_elem.len()-1];
+            let mut found = false;
+            for (entry_key, entry_value) in value.entries() {
+                if entry_key == fieldname {
+                    found = true;
+                    value = entry_value;
+                    break
+                }
+            }
+            if !found {
+                panic!("invalid key");
+            }
+            continue;
+        }
+        if path_elem == "String" {
             return Ok(value.as_str().unwrap().as_bytes().to_vec());
         }
-        if elem == "U64" {
+        if path_elem == "U64" {
             return Ok(value.as_u64().unwrap().to_be_bytes().to_vec());
         }
         panic!("invalid path");
