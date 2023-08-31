35eb1544136eb7194092caeee335675aca4021e8
Rachel Bousfield <nfranks@protonmail.com>
2023-08-30 18:11:30 -0600

    remove warning

--- stylus-sdk/src/debug.rs
+++ stylus-sdk/src/debug.rs
@@ -1,13 +1,11 @@
 // Copyright 2023, Offchain Labs, Inc.
 // For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/stylus/licenses/COPYRIGHT.md
 
-use crate::hostio;
-
 /// Prints a UTF-8 encoded string to the console. Only available in debug mode.
 #[cfg(feature = "debug")]
 pub fn console_log<T: AsRef<str>>(text: T) {
     let text = text.as_ref();
-    unsafe { hostio::log_txt(text.as_ptr(), text.len()) };
+    unsafe { crate::hostio::log_txt(text.as_ptr(), text.len()) };
 }
 
 /// Prints to the console when executing in a debug environment. Otherwise does nothing.
