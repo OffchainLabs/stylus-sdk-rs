20811d580b5cb7dcc6b54854fb8606fdd6af3795
Chris Cordle <chrisco255@protonmail.com>
2023-10-20 09:48:39 -0500

    testing deployment script

---
+++ ci/smoke_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export RUSTFLAGS="-D warnings"
+export RUSTFMT_CI=1
+
+# Print version information
+rustc -Vv
+cargo -V
+cargo stylus --version
+
+# Deploy ERC20 Example
+cd ./examples/erc20
+cargo stylus deploy -e http://localhost:8547 --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659
