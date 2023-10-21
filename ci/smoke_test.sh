3f03aad1a28c0c1f0b48aa49d1bb3b6213634a80
Chris Cordle <chrisco255@protonmail.com>
2023-10-20 19:03:53 -0500

    counter smoke test

--- ci/smoke_test.sh
+++ ci/smoke_test.sh
@@ -7,6 +7,10 @@ rustc -Vv
 cargo -V
 cargo stylus --version
 
-# Deploy ERC20 Example
-cd ./examples/erc20
+# TODO: erc20 example not working
+# cd ./examples/erc20
+
+cargo stylus new counter
+cd counter
+
 cargo stylus deploy -e http://localhost:8547 --private-key 0xb6b15c8cb491557369f3c7d2c287b053eb229daa9c22138887752191c9520659
