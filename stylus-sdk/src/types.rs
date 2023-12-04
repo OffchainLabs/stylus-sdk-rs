d01b3130420c8b422cdb481430b63d4da7f1cc35
cygaar <tmpftl1@gmail.com>
2023-12-03 21:10:58 -0500

    Update

--- stylus-sdk/src/types.rs
+++ stylus-sdk/src/types.rs
@@ -50,6 +50,6 @@ impl AddressVM for Address {
     fn has_code(&self) -> bool {
         let hash = self.codehash();
         !hash.is_zero()
-            || hash == b256!("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470")
+            && hash != b256!("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470")
     }
 }
