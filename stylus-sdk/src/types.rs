e00002948201d0d386a2a4069b230b9a3dd5471e
cygaar <tmpftl1@gmail.com>
2023-12-03 20:13:02 -0500

    Fix Address has_code check

--- stylus-sdk/src/types.rs
+++ stylus-sdk/src/types.rs
@@ -49,7 +49,7 @@ impl AddressVM for Address {
 
     fn has_code(&self) -> bool {
         let hash = self.codehash();
-        hash.is_zero()
+        !hash.is_zero()
             || hash == b256!("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470")
     }
 }
