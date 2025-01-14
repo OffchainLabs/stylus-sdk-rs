6c2f7b079707ce5aaf0b53612b07984b7a6d03b7
Raul Jordan <raul@prysmaticlabs.com>
2025-01-14 10:55:17 -0600

    poc

--- stylus-proc/src/macros/mod.rs
+++ stylus-proc/src/macros/mod.rs
@@ -3,6 +3,7 @@
 
 mod derive;
 mod entrypoint;
+mod proof_of_concept;
 mod public;
 mod sol_interface;
 mod sol_storage;
@@ -12,6 +13,7 @@ pub use derive::abi_type::derive_abi_type;
 pub use derive::erase::derive_erase;
 pub use derive::solidity_error::derive_solidity_error;
 pub use entrypoint::entrypoint;
+pub use proof_of_concept::proof_of_concept;
 pub use public::public;
 pub use sol_interface::sol_interface;
 pub use sol_storage::sol_storage;
