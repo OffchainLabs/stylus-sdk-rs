521f7278295fa10e916b90cd6be9a59c16a82104
bayge <alex@fluidity.money>
2025-04-10 12:42:29 +0300

    Don't use std for stylus-core, lint to ignore alloc_impl unsafe

--- stylus-core/src/lib.rs
+++ stylus-core/src/lib.rs
@@ -1,13 +1,20 @@
 // Copyright 2024-2025, Offchain Labs, Inc.
 // For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
+#![no_std]
 
 //! Defines host environment methods Stylus SDK contracts have access to.
+
+extern crate alloc;
+
 pub mod calls;
 pub mod deploy;
 pub mod host;
 pub mod storage;
 
 use alloy_sol_types::{abi::token::WordToken, SolEvent, TopicList};
+
+use alloc::vec::Vec;
+
 pub use host::*;
 pub use storage::TopLevelStorage;
 
