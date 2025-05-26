f1f27e9c3c72e45832dd04b71493ab134c25dca9
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2025-05-26 15:07:01 -0300

    Add integration tests for single call example

---
+++ examples/single_call/tests/single_call_integration_test.rs
@@ -0,0 +1,51 @@
+// Copyright 2025, Offchain Labs, Inc.
+// For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
+
+#[cfg(feature = "integration-tests")]
+mod integration_test {
+    use alloy::{
+        primitives::{address, Address},
+        sol,
+        sol_types::SolCall,
+    };
+    use eyre::Result;
+    use stylus_tools::devnet::{addresses::OWNER, Node, DEVNET_PRIVATE_KEY};
+
+    const ARB_OWNER_PUBLIC: Address = address!("0x000000000000000000000000000000000000006b");
+
+    sol! {
+        #[sol(rpc)]
+        interface ISingleCall  {
+            function execute(address target, bytes calldata data) external view returns (bytes memory);
+        }
+
+        // ArbOwner precompile used for tests
+        #[sol(rpc)]
+        interface ArbOwnerPublic {
+            function getAllChainOwners() external view returns (address[] memory);
+        }
+    }
+
+    #[tokio::test]
+    async fn single_all() -> Result<()> {
+        let devnode = Node::new().await?;
+        let rpc = devnode.rpc();
+        println!("Deploying contract to Nitro ({rpc})...");
+        let address = stylus_tools::deploy(rpc, DEVNET_PRIVATE_KEY)?;
+        println!("Deployed contract to {address}");
+        let provider = devnode.create_provider().await?;
+        let contract = ISingleCall::ISingleCallInstance::new(address, provider);
+
+        let calldata = ArbOwnerPublic::getAllChainOwnersCall {}.abi_encode();
+        let owners_raw = contract
+            .execute(ARB_OWNER_PUBLIC, calldata.into())
+            .call()
+            .await?;
+        let owners =
+            ArbOwnerPublic::getAllChainOwnersCall::abi_decode_returns_validate(&owners_raw)?;
+
+        assert_eq!(owners, vec![Address::ZERO, OWNER]);
+
+        Ok(())
+    }
+}
