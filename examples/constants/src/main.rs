dae81dbd393bde3bbcdde488b74719c777ddac4c
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2025-05-30 15:09:27 -0300

    Add constants example

---
+++ examples/constants/src/main.rs
@@ -0,0 +1,13 @@
+// Copyright 2025, Offchain Labs, Inc.
+// For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
+
+#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
+
+#[cfg(not(any(test, feature = "export-abi")))]
+#[no_mangle]
+pub extern "C" fn main() {}
+
+#[cfg(feature = "export-abi")]
+fn main() {
+    stylus_constants::print_from_args();
+}
