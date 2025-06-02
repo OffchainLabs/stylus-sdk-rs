1ac7c0cb789b316cb25ff8f9f01f42840b7614ef
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2025-06-02 11:51:46 -0300

    Add abi_decode example

---
+++ examples/abi_decode/src/lib.rs
@@ -0,0 +1,55 @@
+// Copyright 2025, Offchain Labs, Inc.
+// For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
+
+#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
+
+extern crate alloc;
+
+/// Import items from the SDK. The prelude contains common traits and macros.
+use stylus_sdk::{
+    alloy_primitives::{Address, U256},
+    prelude::*,
+};
+// Because the naming of `alloy_primitives` and `alloy_sol_types` is the same, we need to rename the types in `alloy_sol_types`.
+use alloy_sol_types::{
+    sol,
+    sol_data::{Address as SOLAddress, *},
+    SolType,
+};
+
+// Define error
+sol! {
+    error DecodedFailed();
+}
+
+// Error types for the MultiSig contract
+#[derive(SolidityError)]
+pub enum DecoderError {
+    DecodedFailed(DecodedFailed),
+}
+
+#[storage]
+#[entrypoint]
+pub struct Decoder;
+
+/// Declare that `Decoder` is a contract with the following external methods.
+#[public]
+impl Decoder {
+    // This should always return true
+    pub fn encode_and_decode(&self, address: Address, amount: U256) -> Result<bool, DecoderError> {
+        // define sol types tuple
+        type TxIdHashType = (SOLAddress, Uint<256>);
+        // set the tuple
+        let tx_hash_data = (address, amount);
+        // encode the tuple
+        let tx_hash_data_encode = TxIdHashType::abi_encode_params(&tx_hash_data);
+
+        // Check the result
+        match TxIdHashType::abi_decode_params(&tx_hash_data_encode) {
+            Ok(res) => Ok(res == tx_hash_data),
+            Err(_) => {
+                return Err(DecoderError::DecodedFailed(DecodedFailed {}));
+            }
+        }
+    }
+}
---
+++ examples/abi_decode/tests/abi_decode_integration_test.rs
@@ -0,0 +1,35 @@
+// Copyright 2025, Offchain Labs, Inc.
+// For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
+
+#[cfg(feature = "integration-tests")]
+mod integration_test {
+    use alloy::{primitives::{address, U256}, sol};
+    use eyre::Result;
+    use stylus_tools::devnet::{Node, DEVNET_PRIVATE_KEY};
+
+    sol! {
+        #[sol(rpc)]
+        interface IDecoder {
+            function encodeAndDecode(address _address, uint256 amount) external view returns (bool);
+            error DecodedFailed();
+        }
+    }
+
+    #[tokio::test]
+    async fn abi_decode() -> Result<()> {
+        let devnode = Node::new().await?;
+        let rpc = devnode.rpc();
+        println!("Deploying contract to Nitro ({rpc})...");
+        let address = stylus_tools::deploy(rpc, DEVNET_PRIVATE_KEY)?;
+        println!("Deployed contract to {address}");
+        let provider = devnode.create_provider().await?;
+        let contract = IDecoder::IDecoderInstance::new(address, provider);
+
+        let address = address!("0xfafafafafafafafafafafafafafafafafafafafa");
+        let amount = U256::from(1234);
+        let result = contract.encodeAndDecode(address, amount).call().await?;
+        assert!(result);
+
+        Ok(())
+    }
+}
