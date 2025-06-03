78836ba43aa282292d97c866736d7c6bdfca4482
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2025-06-03 14:14:24 -0300

    Add events example

---
+++ examples/events/tests/events_integration_test.rs
@@ -0,0 +1,46 @@
+// Copyright 2025, Offchain Labs, Inc.
+// For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
+
+#[cfg(feature = "integration-tests")]
+mod integration_test {
+    use alloy::{primitives::Address, sol};
+    use eyre::Result;
+    use stylus_tools::devnet::{Node, DEVNET_PRIVATE_KEY};
+
+    sol! {
+        #[sol(rpc)]
+        interface IEvents {
+            function userMain(uint8[] memory _input) external view returns (uint8[] memory);
+        }
+
+        event Log(address indexed sender, string message);
+        event AnotherLog();
+    }
+
+    #[tokio::test]
+    async fn events() -> Result<()> {
+        let devnode = Node::new().await?;
+        let rpc = devnode.rpc();
+        println!("Deploying contract to Nitro ({rpc})...");
+        let address = stylus_tools::deploy(rpc, DEVNET_PRIVATE_KEY)?;
+        println!("Deployed contract to {address}");
+        let provider = devnode.create_provider().await?;
+        let contract = IEvents::IEventsInstance::new(address, provider);
+
+        let tx = contract
+            .userMain(vec![])
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
+
+        let log = tx.decoded_log::<Log>().unwrap();
+        assert_eq!(log.data.sender, Address::from([0x11; 20]));
+        assert_eq!(log.data.message, "Hello world!");
+
+        let another_log = tx.decoded_log::<AnotherLog>();
+        assert!(another_log.is_some());
+
+        Ok(())
+    }
+}
