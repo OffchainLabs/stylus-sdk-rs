cc956aca12dce544ec3182a47e62afd92f3b6a21
Diego Ximenes <dxmendes1@gmail.com>
2025-06-16 11:55:50 -0300

    wip: contract_client_gen macro

--- stylus-proc/src/macros/mod.rs
+++ stylus-proc/src/macros/mod.rs
@@ -1,6 +1,7 @@
 // Copyright 2024, Offchain Labs, Inc.
 // For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
 
+mod contract_client_gen;
 mod derive;
 mod entrypoint;
 mod public;
@@ -8,6 +9,7 @@ mod sol_interface;
 mod sol_storage;
 mod storage;
 
+pub use contract_client_gen::contract_client_gen;
 pub use derive::abi_type::derive_abi_type;
 pub use derive::erase::derive_erase;
 pub use derive::solidity_error::derive_solidity_error;
