ecbd664dfa93da5ed0356c265903de97c97baaa1
Diego Ximenes <dxmendes1@gmail.com>
2025-06-23 14:56:09 -0300

    Refactor input handling in contract-client-gen

--- stylus-proc/src/macros/public/contract_client_gen.rs
+++ stylus-proc/src/macros/public/contract_client_gen.rs
@@ -1,29 +1,34 @@
 // Copyright 2022-2025, Offchain Labs, Inc.
 // For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
 
-use quote::quote;
 use convert_case::{Case, Casing};
+use proc_macro_error::emit_error;
+use quote::quote;
 use sha3::{Digest, Keccak256};
+use syn::spanned::Spanned;
 
-use crate::{
-    consts::STYLUS_CONTRACT_ADDRESS_FIELD,
-    imports::alloy_sol_types::SolType,
-};
+use crate::{consts::STYLUS_CONTRACT_ADDRESS_FIELD, imports::alloy_sol_types::SolType};
 
 fn get_context_and_call(
-    inputs: &syn::punctuated::Punctuated<syn::FnArg, syn::token::Comma>,
-) -> Option<(syn::FnArg, proc_macro2::TokenStream)> {
-    match inputs.iter().next() {
+    first_input: Option<&syn::FnArg>,
+) -> Option<(proc_macro2::TokenStream, proc_macro2::TokenStream)> {
+    match first_input {
         Some(syn::FnArg::Receiver(receiver)) => {
             let is_mutable = receiver.mutability.is_some();
             let is_reference = receiver.reference.is_some();
 
             if is_reference && is_mutable {
                 // &mut self
-                return Some((syn::parse_quote!(context: stylus_sdk::stylus_core::calls::MutatingCallContext), quote!(stylus_sdk::call::call)));
+                return Some((
+                    quote!(stylus_sdk::stylus_core::calls::MutatingCallContext),
+                    quote!(stylus_sdk::call::call),
+                ));
             } else if is_reference {
                 // &self
-                return Some((syn::parse_quote!(context: stylus_sdk::stylus_core::calls::StaticCallContext), quote!(stylus_sdk::call::static_call)));
+                return Some((
+                    quote!(stylus_sdk::stylus_core::calls::StaticCallContext),
+                    quote!(stylus_sdk::call::static_call),
+                ));
             } else {
                 // don't output a method if first argument is not `&self` or `&mut self`
                 return None;
@@ -36,27 +41,35 @@ fn get_context_and_call(
     };
 }
 
-fn get_new_inputs(
-    inputs: &syn::punctuated::Punctuated<syn::FnArg, syn::token::Comma>,
-    context: syn::FnArg,
-) -> syn::punctuated::Punctuated<syn::FnArg, syn::token::Comma> {
-    let mut new_inputs = syn::punctuated::Punctuated::<syn::FnArg, syn::token::Comma>::new();
-    new_inputs.push(syn::parse_quote!(&self));
-    new_inputs.push(syn::parse_quote!(host: &dyn stylus_sdk::stylus_core::host::Host));
-    new_inputs.push(context);
-    new_inputs.extend(inputs.iter().skip(1).cloned());
-    new_inputs
+fn get_inputs_types_and_names<'a>(
+    inputs: impl Iterator<Item = &'a syn::FnArg>,
+) -> (Vec<proc_macro2::TokenStream>, Vec<syn::Ident>) {
+    let mut inputs_types = Vec::new();
+    let mut inputs_names = Vec::new();
+    for input in inputs {
+        match input {
+            syn::FnArg::Typed(pat_type) => {
+                if let syn::Pat::Ident(pat_ident) = &*pat_type.pat {
+                    let ty = &pat_type.ty;
+                    inputs_types.push(quote! { #ty });
+                    inputs_names.push(pat_ident.ident.clone());
+                } else {
+                    emit_error!(input.span(), "Expected identifier in function argument");
+                }
+            }
+            _ => panic!("Expected typed argument"),
+        }
+    }
+    (inputs_types, inputs_names)
 }
 
 pub fn generate_client(item_impl: syn::ItemImpl) -> proc_macro2::TokenStream {
     let client_methods = item_impl.items.iter().filter_map(|impl_item| {
         if let syn::ImplItem::Fn(method) = impl_item {
-            let sig = &method.sig;
-            let method_name = &sig.ident;
-            let inputs = &sig.inputs;
-            let output = &sig.output;
+            let method_name = &method.sig.ident;
+            let output = &method.sig.output;
 
-            let (context, call) = match get_context_and_call(inputs) {
+            let (context, call) = match get_context_and_call(method.sig.inputs.first()) {
                 Some((context, call)) => (context, call),
                 None => {
                     // don't output method
@@ -64,29 +77,9 @@ pub fn generate_client(item_impl: syn::ItemImpl) -> proc_macro2::TokenStream {
                 }
             };
 
-            let new_inputs = get_new_inputs(inputs, context);
+            let inputs = method.sig.inputs.iter().skip(1);
 
-            let rust_input_types = inputs.iter().skip(1).map(|input| {
-                match input {
-                    syn::FnArg::Typed(pat_type) => {
-                        let ty = &pat_type.ty;
-                        quote! { #ty }
-                    }
-                    _ => panic!("Expected typed argument"),
-                }
-            });
-            let rust_input_names = inputs.iter().skip(1).map(|input| {
-                match input {
-                    syn::FnArg::Typed(pat_type) => {
-                        if let syn::Pat::Ident(pat_ident) = &*pat_type.pat {
-                            pat_ident.ident.clone()
-                        } else {
-                            panic!("Expected identifier in function argument");
-                        }
-                    }
-                    _ => panic!("Expected typed argument"),
-                }
-            });
+            let (inputs_types, inputs_names) = get_inputs_types_and_names(inputs.clone());
 
             let rust_output_type = match output {
                 syn::ReturnType::Type(_, ty) => {
@@ -105,7 +98,7 @@ pub fn generate_client(item_impl: syn::ItemImpl) -> proc_macro2::TokenStream {
             let method_name_camel = method_name.to_string().to_case(Case::Camel);
             selector.update(method_name_camel);
             selector.update("(");
-            for (i, input) in inputs.iter().skip(1).enumerate() {
+            for (i, input) in inputs.clone().enumerate() {
                 if i > 0 {
                     selector.update(",");
                 }
@@ -130,10 +123,14 @@ pub fn generate_client(item_impl: syn::ItemImpl) -> proc_macro2::TokenStream {
             let selector3 = selector_bytes[3];
 
             Some(quote! {
-                pub fn #method_name(#new_inputs) -> Result<<#rust_output_type as #SolType>::RustType, stylus_sdk::stylus_core::calls::errors::Error> {
-                    let args = <(#(#rust_input_types,)*) as #SolType>::abi_encode_params(&(#(#rust_input_names,)*));
+                pub fn #method_name(
+                    &self,
+                    host: &dyn stylus_sdk::stylus_core::host::Host,
+                    context: #context, #(#inputs,)*
+                ) -> Result<<#rust_output_type as #SolType>::RustType, stylus_sdk::stylus_core::calls::errors::Error> {
+                    let inputs = <(#(#inputs_types,)*) as #SolType>::abi_encode_params(&(#(#inputs_names,)*));
                     let mut calldata = vec![#selector0, #selector1, #selector2, #selector3];
-                    calldata.extend(args);
+                    calldata.extend(inputs);
                     let call_result = #call(host, context, self.#STYLUS_CONTRACT_ADDRESS_FIELD, &calldata)?;
                     Ok(<#rust_output_type as #SolType>::abi_decode_params(&call_result)?.0)
                 }
