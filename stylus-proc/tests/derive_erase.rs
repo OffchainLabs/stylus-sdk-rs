89903c88022a4ae0bdeae41781f92403c878f251
Diego Ximenes <dxmendes1@gmail.com>
2025-07-01 10:29:35 -0300

    Allow unused imports in stylus-proc tests

--- stylus-proc/tests/derive_erase.rs
+++ stylus-proc/tests/derive_erase.rs
@@ -1,5 +1,9 @@
 // Copyright 2024, Offchain Labs, Inc.
 // For licensing, see https://github.com/OffchainLabs/stylus-sdk-rs/blob/main/licenses/COPYRIGHT.md
+
+// contract-client-gen feature can generate code that makes some imports of this file unused
+#![allow(unused_imports)]
+
 extern crate alloc;
 
 use stylus_proc::{storage, Erase};
