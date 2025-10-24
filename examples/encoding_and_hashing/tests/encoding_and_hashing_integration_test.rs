5e21aef95ac75a3cb83bb5bcf14e48970556a635
Alexandros Filios <afilios@offchainlabs.com>
2025-10-24 13:48:56 +0200

    Extensive testing for all examples

--- examples/encoding_and_hashing/tests/encoding_and_hashing_integration_test.rs
+++ examples/encoding_and_hashing/tests/encoding_and_hashing_integration_test.rs
@@ -23,17 +23,66 @@ mod integration_test {
         }
     }
 
+    const EXPECTED_ABI: &str = "\
+interface IHasher {
+    function encodeAndHash(address target, uint256 value, string calldata func, bytes calldata data, uint256 timestamp) external view returns (bytes32);
+
+    function encodeAndDecode(address _address, uint256 amount) external view returns (bool);
+
+    function packedEncodeAndHash1(address target, uint256 value, string calldata func, bytes calldata data, uint256 timestamp) external view returns (bytes32);
+
+    function packedEncodeAndHash2(address target, uint256 value, string calldata func, bytes calldata data, uint256 timestamp) external view returns (bytes32);
+
+    function encodeWithSignature(string calldata func, address _address, uint256 amount) external view returns (uint8[] memory);
+
+    function encodeWithSignatureAndHash(string calldata func, address _address, uint256 amount) external view returns (bytes32);
+
+    error DecodedFailed();
+}";
+    const EXPECTED_CONSTRUCTOR: &str = "";
+
     #[tokio::test]
     async fn encoding_and_hashing() -> Result<()> {
+        let exporter = stylus_tools::Exporter::builder().build();
+        assert_eq!(exporter.export_abi()?, EXPECTED_ABI);
+        assert_eq!(exporter.export_constructor()?, EXPECTED_CONSTRUCTOR);
+
         let devnode = Node::new().await?;
         let rpc = devnode.rpc();
+
+        println!("Checking contract on Nitro ({rpc})...");
+        stylus_tools::Checker::builder().rpc(rpc).build().check()?;
+        println!("Checked contract");
+
+        let deployer = stylus_tools::Deployer::builder().rpc(rpc).build();
+        println!("Estimating gas...");
+        let gas_estimate = deployer.estimate_gas()?;
+        println!("Estimated deployment gas: {gas_estimate} ETH");
+
         println!("Deploying contract to Nitro ({rpc})...");
-        let (address, _, _) = stylus_tools::Deployer::builder()
-            .rpc(rpc)
-            .build()
-            .deploy()?;
+        let (address, tx_hash, gas_used) = deployer.deploy()?;
         println!("Deployed contract to {address}");
+
+        // Approximate equality is usually expected, but given the test conditions, the gas estimate equals the gas used
+        assert_eq!(gas_used, gas_estimate);
+
+        println!("Activating contract at {address} on Nitro ({rpc})...");
+        stylus_tools::Activator::builder()
+            .rpc(rpc)
+            .contract_address(address.to_string())
+            .build()
+            .activate()?;
+        println!("Activated contract at {address}");
+
+        let verify = stylus_tools::Verifier::builder()
+            .rpc(rpc)
+            .deployment_tx_hash(tx_hash.to_string())
+            .build()
+            .verify();
+        assert!(verify.is_ok(), "Failed to verify contract");
         let provider = devnode.create_provider().await?;
+
+        // Instantiate contract
         let contract = IHasher::IHasherInstance::new(address, provider);
 
         let address = address!("0xfafafafafafafafafafafafafafafafafafafafa");
--- examples/tuples/tests/tuples_integration_test.rs
+++ examples/tuples/tests/tuples_integration_test.rs
@@ -18,17 +18,56 @@ mod integration_test {
         }
     }
 
+    const EXPECTED_ABI: &str = "\
+interface ITuples {
+    function numbers() external returns (uint256, uint256, uint256);
+
+    function bytesAndNumber() external returns (bytes memory, uint256);
+}";
+    const EXPECTED_CONSTRUCTOR: &str = "";
+
     #[tokio::test]
     async fn tuples() -> Result<()> {
+        let exporter = stylus_tools::Exporter::builder().build();
+        assert_eq!(exporter.export_abi()?, EXPECTED_ABI);
+        assert_eq!(exporter.export_constructor()?, EXPECTED_CONSTRUCTOR);
+
         let devnode = Node::new().await?;
         let rpc = devnode.rpc();
+
+        println!("Checking contract on Nitro ({rpc})...");
+        stylus_tools::Checker::builder().rpc(rpc).build().check()?;
+        println!("Checked contract");
+
+        let deployer = stylus_tools::Deployer::builder().rpc(rpc).build();
+        println!("Estimating gas...");
+        let gas_estimate = deployer.estimate_gas()?;
+        println!("Estimated deployment gas: {gas_estimate} ETH");
+
         println!("Deploying contract to Nitro ({rpc})...");
-        let address = stylus_tools::Deployer::builder()
-            .rpc(rpc)
-            .build()
-            .deploy()?;
+        let (address, tx_hash, gas_used) = deployer.deploy()?;
         println!("Deployed contract to {address}");
+
+        // Approximate equality is usually expected, but given the test conditions, the gas estimate equals the gas used
+        assert_eq!(gas_used, gas_estimate);
+
+        println!("Activating contract at {address} on Nitro ({rpc})...");
+        stylus_tools::Activator::builder()
+            .rpc(rpc)
+            .contract_address(address.to_string())
+            .build()
+            .activate()?;
+        println!("Activated contract at {address}");
+
+        let verify = stylus_tools::Verifier::builder()
+            .rpc(rpc)
+            .deployment_tx_hash(tx_hash.to_string())
+            .build()
+            .verify();
+        assert!(verify.is_ok(), "Failed to verify contract");
         let provider = devnode.create_provider().await?;
+
+        // Instantiate contract
         let contract = ITuples::ITuplesInstance::new(address, provider);
 
         let numbers_return = contract.numbers().call().await?;
